79e5098d88aa06f58f391a2cf81fb1b5f2e90b3c
Ayshath Ashfiya A <ayshathashfiyaa@gmail.com>
2024-04-06 11:40:13 +0530

    react component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+// import Greet from './components/Greet';
+//import Message from './components/Messgae';
+// import { ClickHandler } from './components/ClickHandler';
+// import { ParentComponent } from './components/ParentComponent';
+import { UserGreeting } from './components/UserGreeting';
+//import { NameList } from './components/NameList';
+// import { Form } from './components/Form';
+// import Timer from './components/UseEffect';
+// import Parent from './components/Parent';
+import LifecycleExample from './components/LifeCycle';
+// import ControlledComponent from './components/ControlledComponent';
+// import ControlledCheckbox from './components/ControlledComponent';
+ import InputFocus from './components/useRef';
+import CounterUsingUseState from './components/CounterUsingUseState';
+import ReviewUseState from './components/ReviewUseState';
+// import SimpleComponent from './components/SimpleComponent';
+import CondRe from './components/CondRe';
+
+import Counter from './components/Counter';
+
+
+function App()  {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <ReviewUseState/> */}
+      {/* <CounterUsingUseState/> */}
+      {/* <SimpleComponent/> */}
+      <Counter/>
+
+      {/* <Greet name="Aysha" heroName="spiderman"/>
+      <Greet name="Akifa" heroName="superman">
+      <button>action</button>
+      </Greet>
+      <Greet name="Hafiza" heroName="batman">
+        <p>This is children</p>
+        </Greet> */}
+        {/* <Message/> */}
+        {/* <ClickHandler/> */}
+        {/* <ParentComponent/> */}
+        <UserGreeting/>
+        {/* <NameList/> */}
+        {/* <Form/> */}
+        {/* <Timer/>\ */}
+        {/* <Parent/> */}
+        <LifecycleExample/>
+        {/* <ControlledCheckbox/> */}
+        <InputFocus/>
+        <CondRe/>
+       
     </div>
   );
 }
